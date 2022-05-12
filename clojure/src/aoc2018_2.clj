c63d45052a41e002584d01a5f30ebfab81e13bd0
arthur <>
2022-05-12 10:34:01 +0900

    day2 part2 일단 해결
    제대로 이해한게 맞는지는 잘 모르겠음

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -40,26 +40,11 @@
    출력: ...."
   (loop [same-char-count-two 0 same-char-count-three 0 inputs data]
     (if inputs
-      #_{:clj-kondo/ignore [:redundant-do]}
-      (do
-        (let [count-same-char-result (count-same-char (first inputs))]
-          (recur (+ same-char-count-two (if (> (first count-same-char-result) 0) 1 0))
-                 (+ same-char-count-three (if (> (last count-same-char-result) 0) 1 0))
-                 (next inputs))))
+      (let [count-same-char-result (count-same-char (first inputs))]
+        (recur (+ same-char-count-two (if (> (first count-same-char-result) 0) 1 0))
+               (+ same-char-count-three (if (> (last count-same-char-result) 0) 1 0))
+               (next inputs)))
       (* same-char-count-two same-char-count-three))))
-
-(comment
-  (defn get-data
-    "입력: 파일 경로
-     출력: 파일을 읽어서 stirng vector 반환"
-    [filename]
-    (->> (io/resource filename)
-         slurp
-         (str/split-lines)))
-
-  ((day2-part1 (get-data "day2.sample.txt"))
-  ))
-
 
 ;; 파트 2
 ;; 여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.
@@ -74,6 +59,53 @@
 
 ;; 주어진 예시에서 fguij와 fghij는 같은 위치 (2번째 인덱스)에 정확히 한 문자 (u와 h)가 다름. 따라서 같은 부분인 fgij를 리턴하면 됨.
 
+(defn find-same-position-chars
+  " 입력된 두 문자열을 비교하여 같은 index에 있는 문자열을 반환함
+   입력: 원본 문자열, 비교가 될 문자열
+   출력: 원본과 비교하여 동일한 위치에 동일한 문자를 추출하여 하나의 문자열로 만듬"
+  [origin-string target-string]
+  (let [compare-string-bool (map = origin-string target-string)]
+    (str/join
+     (for [index (range (count origin-string))
+           :let [origin-char (nth origin-string index) target-char (nth compare-string-bool index)]]
+       (when target-char origin-char)))))
+
+(defn make-filter-same-length
+  "비교문자열이 계속 증가하다보니 원본 문자열 길이를 넘어서 발생하는 에러를 막기 위해 target-string length를 제한함"
+  [origin-string-length target-string-length]
+  (= origin-string-length (+ 1 (count target-string-length))))
+
+(defn day2-part2
+  "문자열 벡터를 받아서 첫 번째 문자열부터 문자열 백터와 비교하여 같은 포지션에 있는 문자있다면 반환하고 해당 동작을 반복하면서 문자열 생성
+   문제를 정상적으로 이해했는지는 알 수 없음..
+   입력: data는 문자열 백터임"
+  [data]
+  (let [data-string-length (count (first data))
+        filter-length-condition (partial make-filter-same-length data-string-length)]
+    (->> (for [origin-index (range (count data))
+               target-index (range (count data))
+               :let [origin-string (nth data origin-index)
+                     target-string (nth data target-index)]
+               :when (< origin-index target-index)]
+           (find-same-position-chars origin-string target-string))
+         (filter filter-length-condition)
+        ;; (filter (partial #(= data-string-length inc (count %)))) 필터를 따로 안 만들고 넣고 싶었는데
+         first)))
+
+(comment
+  (defn get-data
+    "입력: 파일 경로
+     출력: 파일을 읽어서 stirng vector 반환"
+    [filename]
+    (->> (io/resource filename)
+         slurp
+         (str/split-lines)))
+
+  (day2-part1 (get-data "day2.sample.txt"))
+  (day2-part2 (get-data "day2.sample.txt"))
+  (day2-part2 ["bbczcbd" "abcecbc" "bbcwcbd" "abczcfa" "cbczcbd"])
+  (make-filter-same-length 7 "abcdefghij")
+  (partial make-filter-same-length 7 "abcdefghij"))
 
 ;; #################################
 ;; ###        Refactoring        ###
