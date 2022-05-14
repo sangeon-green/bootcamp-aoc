69b03176e7b2d2bacb3c8b66083d048d46e53aa3
arthur <>
2022-05-14 20:18:02 +0900

    day4 풀어보는 중.. 헷갈리는 중

--- clojure/src/aoc2018_4.clj
+++ clojure/src/aoc2018_4.clj
@@ -29,39 +29,117 @@
 
 ;; 파트 1은 “주어진 입력에 대해서, 가장 오랜시간 잠들어있었던 가드의 ID와, 그 가드가 가장 빈번하게 잠들어 있었던 분(minute)의 곱을 구하라”
 ;; 만약 20번 가드가 0시 10분~36분, 다음날 0시 5분~11분, 다다음날 0시 11분~13분 이렇게 잠들어 있었다면, “11분“이 가장 빈번하게 잠들어 있던 ‘분’. 그럼 답은 20 * 11 = 220.
-
-(defn make-usable-data
+(defn 데이터로드
   "입력: 파일 경로
    출력: (1 740 926 5 4)"
   [filename]
-  (println filename)
   (->> (io/resource filename)
        slurp
        (str/split-lines)
-       ))
+       sort))
 
 
-(defn make-hashmap [변환대상]
-  ((fn [[year month day hour min type id]] {:year year :month month :day day :hour hour :min min :type type :id id}) 변환대상))
+(defn 근무기록-to-데이터 [변환대상]
+  (let [[year month day hour min type id] 변환대상]
+    {:year year :month month :day day :hour hour :min (Integer/parseInt min) :type type :id id}))
 
-(defn pattern []
-  (let [data ((fn [[key x y width heigt]] {:key key :x x :y y :width width :heigt heigt}) (rest (re-find (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (wakes|falls|Guard) #(\d+)" "[1518-11-01 00:00] Guard #10 begins shift"))))]
-    (println data)))
 
-(defn pattern2 [변환대상]
-  (println "변환대상 "  변환대상)
-  (let [data (->> 변환대상
-                  (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (wakes|falls|Guard) #(\d+)")
-                  re-find
-                  rest
-                  make-hashmap)]
-    (println data)))
-(pattern2 "[1518-11-01 00:00] Guard #10 begins shift")
-(make-hashmap "[1518-11-01 00:00] Guard #10 begins shift")
-(map (pattern2 (make-usable-data "day4.sample.txt")))
-(map(make-usable-data "day4.sample.txt"))
-(first (make-usable-data "day4.sample.txt"))
-(make-usable-data "day4.sample.txt")
+(defn 문자열-to-구조체-변화 [변환대상]
+  "입력: [1518-11-01 00:00] Guard #10 begins shift
+   출력: {:year 1518, :month 11, :day 01, :hour 00, :min 00, :type Guard #10, :id 10}"
+  (->> 변환대상
+       (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (falls|wakes|Guard #(\d+))")
+       re-find
+       rest
+       근무기록-to-데이터))
+
+
+(defn 근무자별기록정리 [[모든근무자기록 현재근무자] 근무자기록원본]
+  (if-let [다음근무자 (:id 근무자기록원본)]
+    [(conj 모든근무자기록 {다음근무자 []}) 다음근무자]
+    [(conj (pop 모든근무자기록)
+           {현재근무자 (conj (first (vals (last 모든근무자기록))) (:min 근무자기록원본))})
+     현재근무자]))
+
+
+(defn 취침-기상-시간-seq [근무기록]
+  (println "log" 근무기록)
+  (->> 근무기록
+       (partition 2)
+       (map #(range (first %) (second %)))
+       (reduce concat)))
+
+
+(defn range-to-info [guard list]
+  (println "ragne" guard list)
+  {:guard guard
+   :duration (count list)
+   :frequencies (max-key val (frequencies list))})
+
+
+
+((comment
+   (문자열-to-구조체-변화 "[1518-11-01 00:00] Guard #10 begins shift")
+   (근무기록-to-데이터 "[1518-11-01 00:00] Guard #10 begins shift")
+   (map 문자열-to-구조체-변화 (데이터로드 "day4.sample.txt"))
+ (->> (map 문자열-to-구조체-변화 (데이터로드 "day4.sample.txt"))
+      (reduce 근무자별기록정리 [[] nil])
+      first
+      (apply (partial merge-with into)) ;2
+      )
+   (partition-by #(contains? (set ["[1518-11-01 00:00] Guard #10 begins shift"  "[1518-11-01 23:58] Guard #99 begins shift"]) %) (make-usable-data "day4.sample.txt"))
+   (데이터로드 "day4.sample.txt"))
+ (->> (map 문자열-to-구조체-변화 (데이터로드 "day4.sample.txt"))
+      (reduce 근무자별기록정리 [[] nil])
+      first
+      (apply (partial merge-with into)) ;2
+      (map (fn [[근무자 근무기록]] (range-to-info 근무자 (취침-기상-시간-seq 근무기록))))
+      (sort-by :duration)
+      last
+      ))
+
+
+
+
+;; 조금더 예쁘게 바꿔보는 중
+;; (defn pattern []
+;;   (let [data ((fn [[key x y width heigt]] {:key key :x x :y y :width width :heigt heigt}) (rest (re-find (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (wakes|falls|Guard) #(\d+)" "[1518-11-01 00:00] Guard #10 begins shift"))))]
+;;     (println data)))
+
+;; partition-by를 쓰고 싶었지만 실패한 흔적들..
+;; (->> (make-usable-data "day4.sample.txt")
+;;      (partition-by #(= re-find (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] Guard #(\d+) begins shift") %))
+;; )
+;; (
+;;  (re-find (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] Guard #(\d+) begins shift" "[1518-11-01 00:00] Guard #10 begins shift"))
+;; )
+
+;; (defn filter [대상]
+;;   (->> 대상
+;;        (re-matcher #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] Guard #(\d+) begins shift")
+;;        re-find
+;; ))
+;; (filter "[1518-11-01 00:00] Guard #10 begins shif")
+;; (
+;;  (let [a -> ((map #(filter) % (make-usable-data "day4.sample.txt")))]
+;;    a)
+;; )
+;; (defn 가드-아이디-업데이트 [업데이트대상]
+    ;;  (println 업데이트대상)
+    ;;  (let [value (->> (for [index (range (count 업데이트대상))]
+    ;;                     (if (nil? (:id (nth 업데이트대상 index)))
+    ;;                       (-> (assoc (nth 업데이트대상 index) :id ((nth 업데이트대상 (dec index)) :id)))
+    ;;                       (nth 업데이트대상 index))))]
+    ;;    value))
+
 ;; 파트 2
 ;; 주어진 분(minute)에 가장 많이 잠들어 있던 가드의 ID과 그 분(minute)을 곱한 값을 구하라.
+
+
+;; {id: aa, value: [ time 00:00
+;;                  type 슬립
+;;                  time: 00:05
+;;                  tye: wake] }
+
+;; sort 
 
