7489dacca5a1c3d55cfd3a8ac5aa0cd0417f71fe
arthur <>
2022-05-20 14:38:33 +0900

    2018-day7 데이터 구조는 만듬

---
+++ clojure/src/aoc2018_7.clj
@@ -0,0 +1,34 @@
+(ns aoc2018-7
+  (:require [clojure.core]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.data :as data]
+            [clojure.set :as set]))
+
+(defn 선행-후행-업무분리
+  "입력된 값을 분석하여 현재 업무를 기준으로 선행되어야 할 업무를 입력
+   입력값: Step C must be finished before step A can begin.
+   출력값: {A #{C}}"
+  [입력값]
+  (->> 입력값
+       (re-seq #"(\W) must be finished before step (\W)) can begin.")
+       (map #(rest %))
+       (reduce
+         (fn [업무분리 [선행업무 후행업무]]
+           (if (contains? 업무분리 후행업무)
+             (update 업무분리 후행업무 (fn [v] (conj v 선행업무)))
+             (assoc 업무분리 후행업무 (sorted-set 선행업무)))) {})))
+
+(def data-load
+  (->>  (io/resource "day7.sample-light.txt")
+        slurp
+        선행-후행-업무분리))
+
+(defn 최초시작업무검색
+  "선행 업무가 없는 업무를 검색합니다."
+  [전체업무]
+  (let [선행업무
+        (->>
+         (data/diff (apply set/union (vals 전체업무)) (set (keys 전체업무)))
+         first)]
+    선행업무))
